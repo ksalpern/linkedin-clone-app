bb8a727a393e4a512f42872a3d36604d498e114f
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-16 12:49:45 +0200

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
     return (
-        <div className='header'>
-            <h1>Header </h1>
-        </div>
+        <header className='header'>
+            <div className="header__left">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" alt="" />
+
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
+            <div className="header__right">
+
+            </div>
+        </header>
     )
 }
 
