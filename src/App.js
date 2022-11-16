7542da9c1ded32a636d502fbbee95c93303ee843
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-16 12:19:37 +0200

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
-<h1>LinkedIn Clone</h1>
+      <Header />
     </div>
   );
 }
