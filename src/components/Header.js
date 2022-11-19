e03479c5c27a57d38333ee19b7eeaa950ded4b90
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-19 15:50:20 +0200

    update HeaderOption

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home'; import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header() {
     return (
@@ -18,10 +21,9 @@ function Header() {
             <div className="header__right">
                 <HeaderOption Icon={HomeIcon} title='Home' />
                 <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
-                <HeaderOption title='Home' />
-                <HeaderOption title='Home' />
-                <HeaderOption title='Home' />
-                <HeaderOption title='Home' />
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+                <HeaderOption Icon={ChatIcon} title='Messaging' />
+                <HeaderOption Icon={NotificationsIcon} title='Notificatios' />
             </div>
         </header>
     )
