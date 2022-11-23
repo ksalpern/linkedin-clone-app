a038de8394b250fb4f5496094757ab91613b2064
Kseniia Pi <ksenia.pidopryhora@gmail.com>
2022-11-23 11:51:11 +0200

    add InputOption component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,25 @@
 import React from "react";
-import "Feed.css";
+import "./Feed.css";
+import CreateIcon from "@mui/icons-material/Create";
+import InputOption from './InputOption';
 
 function Feed() {
-  return <div className="feed"></div>;
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button>Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
---
+++ src/components/InputOption.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./InputOption.css";
+
+function InputOption() {
+  return <div className="option"></div>;
+}
+
+export default InputOption;
