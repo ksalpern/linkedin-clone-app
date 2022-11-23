b5816695d0ddceb6f5de1cafe2869c8cb67fe729
Kseniia Pi <ksenia.pidopryhora@gmail.com>
2022-11-23 12:59:13 +0200

    update InputOption

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Feed.css";
 import CreateIcon from "@mui/icons-material/Create";
-import InputOption from './InputOption';
+import InputOption from "./InputOption";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 
 function Feed() {
   return (
@@ -15,7 +19,14 @@ function Feed() {
           </form>
         </div>
         <div className="feed__inputOptions">
-          <InputOption />
+          <InputOption title="Photo" Icon={ImageIcon} color="#70b5f9" />
+          <InputOption title="Video" Icon={SubscriptionsIcon} color="#e7a33e" />
+          <InputOption title="Event" Icon={EventNoteIcon} color="#c0cbcd" />
+          <InputOption
+            title="Write article"
+            Icon={CalendarViewDayIcon}
+            color="#7fc15e"
+          />
         </div>
       </div>
     </div>
