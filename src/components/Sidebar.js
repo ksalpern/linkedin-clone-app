323eadec8bdb72222075589167e15df7bd2e765a
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-29 01:27:26 +0200

    update user

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,12 @@
 import { Avatar } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const user = useSelector(selectUser)
+
   const recentItem = (topic) => (
     <div className="sidebar__recentItem">
       <span className="sidebar__hash">#</span>
@@ -17,9 +21,9 @@ function Sidebar() {
           src="https://static-exp1.licdn.com/sc/h/55k1z8997gh8dwtihm11aajyq"
           alt=""
         />
-        <Avatar className="sidebar__avatar" />
-        <h2>Kseniia P</h2>
-        <h4>ksenia.pidopryhora@gmail.com</h4>
+        <Avatar className="sidebar__avatar" src={user.photoUrl}>{user.email[0]}</Avatar>
+        <h2>{user.displayName}</h2>
+        <h4>{user.email}</h4>
       </div>
 
       <div className="sidebar__stats">
